6af66acf4f0fd98b2f8727feb26fddd8e965c449
Guzenko-Alexandr <sasha5539107@mail.ru>
2021-03-14 16:51:09 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+  if (!array.length) {
+    return 0;
+  }
+  let minElement = Infinity;
+  array.forEach(element => {
+    if (element < minElement) {
+      minElement = element;
+    }
+  });
+  if (minElement == Infinity) {
+    return 0;
+  }
+  return minElement;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+  if (!array.length) {
+    return 0;
+  }
+  let maxElement = -Infinity;
+  array.forEach(element => {
+    if (element > maxElement) {
+      maxElement = element;
+    }
+  });
+  if (maxElement == -Infinity) {
+    return 0;
+  }
+  return maxElement;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+  if (!array.length) {
+    return 0;
+  }
+  let sum = 0;
+  array.forEach(element => {
+    sum += element;
+  })
+  if (sum == 0) {
+    return 0;
+  }
+  let mean = sum / array.length;
+  return mean;
 }
